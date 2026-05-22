1111042f95c61c16b125f7f82cac28d5ef61e122
victor <1918967@alu.murciaeduca.es>
2026-05-22 15:52:39 +0200

    cambios del cliente

--- obj/Debug/net8.0/TallerBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/TallerBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TallerBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d17fb7ee4ba9ad4db27b4ab1aad1388fcb6d5803")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b80d128d0b50744a0f82b46b5ec70ff17623af5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TallerBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TallerBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
